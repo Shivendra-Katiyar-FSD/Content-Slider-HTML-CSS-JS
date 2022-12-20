618a741ea3a46eb9fff38991d4d11ac09855b8db
Shivendra-Katiyar-FSD <katiyar.shivendra82@gmail.com>
2022-12-21 02:25:09 +0530

    Content Slider

---
+++ script.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+        1275: {
+          slidesPerView: 4,
+        },
+        1500: {
+          slidesPerView: 5,
+        }
+    },
+  });
